29cac01731820a30e8be8f43d907bc3771b751bf
RJMALLU <98645849+RJMALLU@users.noreply.github.com>
2022-05-06 15:07:00 +0400

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,8 +14,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ" ,url="https://t.me/publicchannalin") ], 
+	[InlineKeyboardButton("ɢʀᴏᴜᴩ", url="https://t.me/cine_makotta") ]  ]))
 
 
 
